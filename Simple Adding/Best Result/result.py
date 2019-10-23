1d77db0b96b5839953f99680d6fcbb5edb61fdfb
Nicholas Synovic <nicholas.synovic@gmail.com>
2019-10-23 15:59:39 -0500

    Uploaded the best result
    This was the best result for the question Simple Adding by user vmegas

---
+++ Simple Adding/Best Result/result.py
@@ -0,0 +1,12 @@
+def SimpleAdding(num): 
+
+    # code goes here 
+    result = 0
+    
+    for i in range(num+1):
+      result += i
+      
+    return result
+    
+# keep this function call here  
+print(SimpleAdding(input()))
