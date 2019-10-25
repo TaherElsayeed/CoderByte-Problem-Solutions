fe3b99405de545fac360d8524644f16533a58cc9
Nicholas Synovic <nicholas.synovic@gmail.com>
2019-10-25 00:20:02 -0500

    Update result.py
    Changed the name to better reflect the script's purpose
    Added the problem URL
    Added the question
    Added comments to make the code more understandable

--- Time Convert/result.py
+++ Time Convert/result.py
@@ -1,11 +1,16 @@
+#	Problem URL:	https://coderbyte.com/editor/Time%20Convert:JavaScript
+#	Question:	Have the function TimeConvert(num) take the num parameter being passed and return the number of hours and minutes 
+#	the parameter converts to (ie. if num = 63 then the output should be 1:3). Separate the number of hours and minutes with a 
+#	colon. 
+
 def TimeConvert(num):
-  
-  # code goes here
-  hours = 0
-  minutes = 0
-  hours = num // 60
-  minutes = num % 60
-  return str(hours) + ":" + str(minutes)
+
+	# code goes here
+	hours = 0
+	minutes = 0
+	hours = num // 60	#	Floor division
+	minutes = num % 60	#	Mod operation
+	return str(hours) + ":" + str(minutes)
 
 # keep this function call here 
 print(TimeConvert(input()))
