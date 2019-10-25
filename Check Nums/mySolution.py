bdc0e289458e55e5093f127f5500ba23d918092e
Nicholas Synovic <nicholas.synovic@gmail.com>
2019-10-24 22:49:48 -0500

    Update and rename result.py to mySolution.py
    Changed the name of the script to reflect what it is
    Added the problem URL
    Added the question itself
    Changed the elif to an if in order to make it more pythonic

---
+++ Check Nums/mySolution.py
@@ -0,0 +1,16 @@
+#	Problem URL:	https://coderbyte.com/editor/Check%20Nums:Python3
+#	Question:
+# 	Have the function CheckNums(num1,num2) take both parameters being passed and return the string true if num2 is greater than
+#	num1, otherwise return the string false. If the parameter values are equal to each other then return the string -1.
+
+def CheckNums(num1,num2):
+
+	# code goes here
+	if num1 < num2:
+		return True
+	if num1 > num2:
+		return False
+	return -1
+
+# keep this function call here 
+print(CheckNums(input()))
