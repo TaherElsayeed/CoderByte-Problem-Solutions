82f75efe89e32d6754eef848be5f1a5af8e1e307
Nicholas Synovic <nicholas.synovic@gmail.com>
2019-10-24 22:57:16 -0500

    Update and rename result.py to mySolution.py
    Changed the name of the script to better reflect what it is
    Added the problem URL
    Added the question
    Removed some comments to better show what I was doing
    Added a better return call to keep the code cleaner

--- First Reverse/mySolution.py
+++ First Reverse/mySolution.py
@@ -1,16 +1,20 @@
+#	Problem URL:	https://coderbyte.com/editor/First%20Reverse:Python3
+#	Question:
+#	Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. For example: 
+#	if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH. 
+
 def FirstReverse(str):
 
 # code goes here 
-	foo = len(str)	# Gets the length of the input
-	bar = []	# Creates an empty list to store the chars of the str in reverse order
+foo = len(str)
+bar = []
 	for x in range(foo):	# Iterates over the length of the input and appends the letters in reverse order
 		if x == 0:	# Skips over the first instance because 0 is not a negative numbeer
 			pass
 		else:
 			bar.append(str[-x])
 	bar.append(str[0])	# Appends the first letter of the str to the end of the array
-	faz = ''.join(bar)	#	Creates one string from the array of chars
-	return faz	# Returns the new string
+	return ''.join(bar)	# Returns a new string that is the combination of all of the chars in the list
 
 # keep this function call here
 print(FirstReverse(input()))	# Prints the output of the function and takes a user input as well
