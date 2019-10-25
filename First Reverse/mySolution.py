4a902ce396f65cd9f6ac83c3a42219c08502bca4
Nicholas Synovic <nicholas.synovic@gmail.com>
2019-10-25 00:41:51 -0500

    Adjusted Comment Spacing

--- First Reverse/mySolution.py
+++ First Reverse/mySolution.py
@@ -8,13 +8,13 @@ def FirstReverse(str):
 # code goes here 
 foo = len(str)
 bar = []
-	for x in range(foo):	# Iterates over the length of the input and appends the letters in reverse order
-		if x == 0:	# Skips over the first instance because 0 is not a negative numbeer
+	for x in range(foo):	#	Iterates over the length of the input and appends the letters in reverse order
+		if x == 0:	# 	Skips over the first instance because 0 is not a negative numbeer
 			pass
 		else:
 			bar.append(str[-x])
-	bar.append(str[0])	# Appends the first letter of the str to the end of the array
-	return ''.join(bar)	# Returns a new string that is the combination of all of the chars in the list
+	bar.append(str[0])	#	Appends the first letter of the str to the end of the array
+	return ''.join(bar)	# 	Returns a new string that is the combination of all of the chars in the list
 
 # keep this function call here
-print(FirstReverse(input()))	# Prints the output of the function and takes a user input as well
+print(FirstReverse(input()))	# 	Prints the output of the function and takes a user input as well
