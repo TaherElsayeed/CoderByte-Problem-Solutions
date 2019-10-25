a2710e0c84cf6d8277e734de2d5638d98252f5f3
Nicholas Synovic <nicholas.synovic@gmail.com>
2019-10-24 22:42:56 -0500

    Added Comments
    Added the question URL
    Added the question itself
    Added comments to my code to make it readable

--- Alphabet Soup/result.py
+++ Alphabet Soup/result.py
@@ -1,12 +1,17 @@
+#	Problem URL: https://coderbyte.com/information/Alphabet%20Soup
+#	Question:
+#	Have the function AlphabetSoup(str) take the str string parameter being passed and return the string with the letters in
+#	alphabetical order (ie. hello becomes ehllo). Assume numbers and punctuation symbols will not be included in the string.
+
 def AlphabetSoup(str):
 
-  # code goes here
-  data = []
-  for x in range(len(str)):
-    data.append(str[x])
-    
-  data = sorted(data)
-  return ''.join(data)
+	# code goes here
+	data = []
+	for x in range(len(str)):	# Turns a string into a list where each index is where a char is located
+	data.append(str[x])
+
+	data = sorted(data)	#	Sorts the list in alphabetical order
+	return ''.join(data)	#	Combines the list into a stirng
 
 # keep this function call here 
 print(AlphabetSoup(input()))
