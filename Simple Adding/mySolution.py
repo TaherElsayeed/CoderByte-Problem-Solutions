d81b5c494e2d509ef2dccb889cd3d563b672f5b9
Nicholas Synovic <nicholas.synovic@gmail.com>
2019-10-25 00:07:46 -0500

    Update and rename result.py to mySolution.py
    Changed the name to better reflect the script's purpose
    Added Problem URL
    Added question
    Added comments to the code to make it more readable

---
+++ Simple Adding/mySolution.py
@@ -0,0 +1,24 @@
+#	This program assumes that the arguement num is a positive integer
+#	Problem URL:	https://coderbyte.com/editor/Simple%20Adding:Python3
+#	Question:
+#	Have the function SimpleAdding(num) add up all the numbers from 1 to num. For example: if the input is 4 then your program 
+#	should return 10 because 1 + 2 + 3 + 4 = 10. For the test cases, the parameter num will be any number from 1 to 1000. 
+
+def SimpleAdding(num):
+
+	# code goes here
+	data = []
+	
+	for x in range(num):	#	Generates a list of every number from 0 to the arguement including the arguement
+		data.append(x)
+	data.append(num)
+
+	sum = 0
+	
+	for x in data:	#	Adds all of the values in the list of every number
+		sum += x
+	
+	return sum
+
+# keep this function call here 
+print(SimpleAdding(input()))
